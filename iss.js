19c1b2f899f36ddd6b04fbae1cc9de50b29f434f
Ahmed Tarabia <ahmedtarabia@gmail.com>
2021-09-23 19:55:47 -0400

    nested / callback hell

--- iss.js
+++ iss.js
@@ -1,13 +1,19 @@
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- */
 
 const request = require('request');
+
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    fetchCoordsByIP(ip, (error, coords) => {
+      fetchISSFlyOverTimes(coords,(error, passTimes) => {
+        if (error) {
+          return callback(error);
+        }
+        callback(error, passTimes) 
+        
+      })
+    })
+  })      
+}
 
 const fetchMyIP = function(callback) {
   const url = "https://api.ipify.org/?format=json";
@@ -29,6 +35,47 @@ const fetchMyIP = function(callback) {
       callback(null, data.ip);
     }
   });
-};
+}
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) { //ip string
+  const url = `https://freegeoip.app/json/${ip}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+      // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+      // if we get here, all's well and we got the data
+    const { latitude, longitude } = JSON.parse(body);
+    callback(null, { latitude, longitude });
+  });
+}
+
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const url = `https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+      // if non-200 status, assume server error
+      if (response.statusCode !== 200) {
+        const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+        callback(Error(msg), null);
+        return;
+      }
+      // if we get here, all's well and we got the data
+      const data = JSON.parse(body);
+      if (data.response) {
+        callback(null, data.response);
+        
+      }
+    });
+  }
+
+
+
+module.exports = { nextISSTimesForMyLocation };
